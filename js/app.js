64273a0502c5c4973fd124655e20b1e16c485107
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-07-07 16:58:23 +0430

    handleMathOprator/kimiaGoudarzi

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
 const result = document.getElementById('result');
+let flagPow = false;
+let resultPow = 0 ;
 function addToCal(inputValue) {
   result.value += inputValue;
+  if(flagPow == true){
+   let numbers = result.value.split("^");
+   resultPow += Math.pow(numbers[0],numbers[1]);
+  }
 }
 
 function final(){
+  if(flagPow){
+    console.log(resultPow);
+    result.value = resultPow;
+    flagPow = false;
+  }else if(result.value){
     result.value = eval(result.value);
+  }
+  else{
+    result.value = " "
+  } 
 }
 
 function deleteCal(){
-    result.value = 0;
+    result.value = "";
 }
+
+function handleMathOprator(mathName){
+  if(mathName=="pow"){
+    flagPow = true;
+    result.value += "^";
+  }else{
+    result.value = Math[mathName](result.value);
+  } 
+}
+
 
 
